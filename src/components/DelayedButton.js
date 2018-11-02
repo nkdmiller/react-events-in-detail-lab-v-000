db71e5f136807c8fd3d1a467182c6e5b9de8c210
Nate Miller <natekmiller96@gmail.com>
2018-11-02 19:34:53 +0000

    automatically backed up by learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 
 export default class CoordinatesButton extends Component {
   clickButton = (e) => {
-    const coordinates = [e.clientX, e.clientY]
+    setTimeout()
     this.props.onReceiveCoordinates(coordinates)
   }
    render(){
