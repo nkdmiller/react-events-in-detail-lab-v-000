40d96136241059d5cf3f8fec5bcac4ca6fb8f9e6
Nate Miller <natekmiller96@gmail.com>
2018-11-02 19:29:15 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 
 export default class CoordinatesButton extends Component {
-  onReceiveCoordinates = () => {console.log('Entering password...')};
+  onReceiveCoordinates = (x,y) => {console.log('Entering password...')};
   render () {
     return(
   <input onKeyUp={this.typing} type="password" />)
